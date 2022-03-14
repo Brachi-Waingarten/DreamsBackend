0601b590ccef90fa250d08dadf5166ed49d1661a
Brachi-Waingarten <b0527680507@gmail.com>
2022-03-14 18:51:37 +0200

    add schema

--- models/item.js
+++ models/item.js
@@ -3,7 +3,7 @@ const mongoose=require("mongoose");
 const itemSchema=new mongoose.Schema({
     item_name:String,
     item_description:String,
-    item_price:{type:Number,min:0}
+    item_pricee:{type:Number,min:0}
 })
 
 const Item=mongoose.model("items",itemSchema);
